d3030a22c250aa8892554722d9f28dc28559f278
Abhiyan Khanal <abhiyan5588@gmail.com>
2021-03-05 06:18:11 +0545

    first

---
+++ lib/controller/horoscope.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Map data;
+
+class HoroscopeContriller extends StatefulWidget {
+  @override
+  _HoroscopeContrillerState createState() => _HoroscopeContrillerState();
+}
+
+class _HoroscopeContrillerState extends State<HoroscopeContriller> {
+  var aries,
+      taurus,
+      gemini,
+      cancer,
+      leo,
+      virgo,
+      libra,
+      sagittarius,
+      capricorn,
+      aquarius,
+      pisces,
+      scorpio,
+      rashi;
+  Future fetch(rashi) async {
+    http.Response response = await http.get(
+      "http://horoscope-api.herokuapp.com/horoscope/today/" + rashi,
+    );
+    setState(() {
+      data = jsonDecode(response.body);
+    });
+    return data;
+  }
+
+  Future load() async {
+    aries = await fetch("aries");
+    taurus = await fetch("taurus");
+    gemini = await fetch("gemini");
+    cancer = await fetch("cancer");
+    leo = await fetch("leo");
+    virgo = await fetch("virgo");
+    libra = await fetch("libra");
+    sagittarius = await fetch("SAGITTARIUS");
+    capricorn = await fetch("CAPRICORN");
+    aquarius = await fetch("AQUARIUS");
+    pisces = await fetch("PISCES");
+    scorpio = await fetch("scorpio");
+    print('data fetched successfully');
+  }
+
+  @override
+  void initState() {
+    load();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (data == null) {
+      return CircularProgressIndicator();
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(data.toString()),
+        ),
+      );
+    }
+  }
+}
---
+++ lib/models/horoscope.dart
@@ -0,0 +1,11 @@
+class Horo {
+  var date;
+  String horoscope;
+  String sunshine;
+  Horo(this.date, this.horoscope, this.sunshine);
+  Horo.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    horoscope = json['horoscope'];
+    sunshine = json['sunshine'];
+  }
+}
