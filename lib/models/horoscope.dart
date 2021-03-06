1d4a748ad871944c8e8fb32db43275da1704062d
Abhiyan Khanal <abhiyan5588@gmail.com>
2021-03-06 21:04:34 +0545

    list_view_implemented

--- lib/models/horoscope.dart
+++ lib/models/horoscope.dart
@@ -1,11 +1,12 @@
-// class Horo {
-//   var date;
-//   String horoscope;
-//   String sunshine;
-//   Horo(this.date, this.horoscope, this.sunshine);
-//   Horo.fromJson(Map<String, dynamic> json) {
-//     date = json['date'];
-//     horoscope = json['horoscope'];
-//     sunshine = json['sunshine'];
-//   }
-// }
+class Horo {
+  var date;
+  String horoscope;
+  String sunshine;
+  String img_url;
+  Horo(this.date, this.horoscope, this.sunshine, this.img_url);
+  // Horo.fromJson(Map<String, dynamic> json) {
+  //   date = json['date'];
+  //   horoscope = json['horoscope'];
+  //   sunshine = json['sunshine'];
+  // }
+}
--- lib/src/horoscope.dart
+++ lib/src/horoscope.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:new_jyotish/constants/color.dart';
 import 'package:http/http.dart' as http;
-// import 'package:new_jyotish/models/horoscope.dart';
+import 'package:new_jyotish/models/horoscope.dart';
 
 class Horoscope extends StatefulWidget {
   @override
@@ -12,7 +12,21 @@ class Horoscope extends StatefulWidget {
 
 class _HoroscopeState extends State<Horoscope> {
   Map<String, dynamic> data;
-  List<Map<String, dynamic>> horo = [];
+  List horo = [
+    "aries",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "libra",
+    "sagittarius",
+    "capricorn",
+    "aquarius",
+    "pisces",
+    "scorpio"
+  ];
+  //List img_url = ["", "", "", "", "", "", "", "", "", "", "",""];
   String rashi;
   Future fetch(rashi) async {
     http.Response response = await http.get(
@@ -27,7 +41,7 @@ class _HoroscopeState extends State<Horoscope> {
   }
 
   Future load() async {
-    horo[1] = await fetch("aries");
+    horo[0] = await fetch("aries");
     horo[1] = await fetch("taurus");
     horo[2] = await fetch("gemini");
     horo[3] = await fetch("cancer");
@@ -54,241 +68,170 @@ class _HoroscopeState extends State<Horoscope> {
     double _width = MediaQuery.of(context).size.width;
     if (horo[11] != null) {
       return Scaffold(
-        body: SingleChildScrollView(
-          child: Stack(overflow: Overflow.visible, children: <Widget>[
-            Container(
-              height: _height * 0.2,
-              decoration: BoxDecoration(
-                color: mainCol,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(60.0),
-                  bottomLeft: Radius.circular(60.0),
-                ),
+        body: Stack(overflow: Overflow.visible, children: <Widget>[
+          Container(
+            height: _height * 0.2,
+            decoration: BoxDecoration(
+              color: mainCol,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(60.0),
+                bottomLeft: Radius.circular(60.0),
               ),
             ),
-            Positioned(
-              left: _width * 0.35,
-              top: _height * 0.06,
-              child: Text(
-                "Horoscope",
-                style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white),
-              ),
+          ),
+          Positioned(
+            left: _width * 0.35,
+            top: _height * 0.06,
+            child: Text(
+              "Horoscope",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
             ),
-            Positioned(
-              top: _height * 0.1,
+          ),
+          Positioned(
+              top: _height * 0.08,
               left: _width * 0,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: _width * 0.95,
-                    margin: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: cardCol,
-                      boxShadow: [
-                        BoxShadow(
-                          color: shadowCol.withOpacity(0.7),
-                          //spreadRadius: 2,
-                          blurRadius: 9,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            height: _height * 0.13,
-                            width: _width * 0.2,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              image: DecorationImage(
-                                  image: AssetImage("assets/1.jpg"),
-                                  fit: BoxFit.cover),
-                            ),
+              child: Container(
+                height: _height,
+                width: _width,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: horo.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: <Widget>[
+                        Container(
+                          width: _width * 0.95,
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: cardCol,
+                            boxShadow: [
+                              BoxShadow(
+                                color: shadowCol.withOpacity(0.7),
+                                //spreadRadius: 2,
+                                blurRadius: 9,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 25.0),
-                          Flexible(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Row(
                               children: <Widget>[
-                                Text(
-                                  horo[11]['sunsign'].toUpperCase(),
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.w400,
-                                    color: mainCol,
+                                Container(
+                                  height: _height * 0.15,
+                                  width: _width * 0.2,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: AssetImage("assets/1.jpg"),
+                                        fit: BoxFit.cover),
                                   ),
                                 ),
-                                SizedBox(height: 5.0),
-                                Text(
-                                  horo[11]['horoscope'],
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 13.0,
+                                SizedBox(width: 25.0),
+                                Flexible(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        horo[index]['sunsign'].toUpperCase(),
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w400,
+                                          color: mainCol,
+                                        ),
+                                      ),
+                                      SizedBox(height: 3.0),
+                                      Text(
+                                        horo[index]['horoscope'],
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 13.0,
+                                        ),
+                                        // maxLines: 4,
+                                      ),
+                                    ],
                                   ),
-                                  maxLines: 3,
-                                ),
+                                )
                               ],
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  // Container(
-                  //   width: _width * 0.95,
-                  //   margin: EdgeInsets.all(10.0),
-                  //   decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.circular(10.0),
-                  //     color: cardCol,
-                  //     boxShadow: [
-                  //       BoxShadow(
-                  //         color: shadowCol.withOpacity(0.7),
-                  //         //spreadRadius: 2,
-                  //         blurRadius: 9,
-                  //         offset: Offset(0, 3), // changes position of shadow
-                  //       ),
-                  //     ],
-                  //   ),
-                  //   child: Padding(
-                  //     padding: const EdgeInsets.all(10.0),
-                  //     child: Row(
-                  //       children: <Widget>[
-                  //         Container(
-                  //           height: _height * 0.13,
-                  //           width: _width * 0.2,
-                  //           decoration: BoxDecoration(
-                  //             borderRadius: BorderRadius.circular(10.0),
-                  //             image: DecorationImage(
-                  //                 image: AssetImage("assets/1.jpg"),
-                  //                 fit: BoxFit.cover),
-                  //           ),
-                  //         ),
-                  //         SizedBox(width: 25.0),
-                  //         Column(
-                  //           crossAxisAlignment: CrossAxisAlignment.start,
-                  //           children: <Widget>[
-                  //             Text(
-                  //               "Abhiyan Khanal",
-                  //               style: TextStyle(
-                  //                   fontSize: 17.0,
-                  //                   fontWeight: FontWeight.w400,
-                  //                   color: mainCol),
-                  //             ),
-                  //             SizedBox(height: 5.0),
-                  //             Text("Ekantakuna, Kathmandu"),
-                  //           ],
-                  //         )
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
-                  // Container(
-                  //   width: _width * 0.95,
-                  //   margin: EdgeInsets.all(10.0),
-                  //   decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.circular(10.0),
-                  //     color: cardCol,
-                  //     boxShadow: [
-                  //       BoxShadow(
-                  //         color: shadowCol.withOpacity(0.7),
-                  //         //spreadRadius: 2,
-                  //         blurRadius: 9,
-                  //         offset: Offset(0, 3), // changes position of shadow
-                  //       ),
-                  //     ],
-                  //   ),
-                  //   child: Padding(
-                  //     padding: const EdgeInsets.all(10.0),
-                  //     child: Row(
-                  //       children: <Widget>[
-                  //         Container(
-                  //           height: _height * 0.13,
-                  //           width: _width * 0.2,
-                  //           decoration: BoxDecoration(
-                  //             borderRadius: BorderRadius.circular(10.0),
-                  //             image: DecorationImage(
-                  //                 image: AssetImage("assets/1.jpg"),
-                  //                 fit: BoxFit.cover),
-                  //           ),
-                  //         ),
-                  //         SizedBox(width: 25.0),
-                  //         Column(
-                  //           crossAxisAlignment: CrossAxisAlignment.start,
-                  //           children: <Widget>[
-                  //             Text(
-                  //               "Abhiyan Khanal",
-                  //               style: TextStyle(
-                  //                   fontSize: 17.0,
-                  //                   fontWeight: FontWeight.w400,
-                  //                   color: mainCol),
-                  //             ),
-                  //             SizedBox(height: 5.0),
-                  //             Text("Ekantakuna, Kathmandu"),
-                  //           ],
-                  //         )
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
-                  // Container(
-                  //   width: _width * 0.95,
-                  //   margin: EdgeInsets.all(10.0),
-                  //   decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.circular(10.0),
-                  //     color: cardCol,
-                  //     boxShadow: [
-                  //       BoxShadow(
-                  //         color: shadowCol.withOpacity(0.7),
-                  //         //spreadRadius: 2,
-                  //         blurRadius: 9,
-                  //         offset: Offset(0, 3), // changes position of shadow
-                  //       ),
-                  //     ],
-                  //   ),
-                  //   child: Padding(
-                  //     padding: const EdgeInsets.all(10.0),
-                  //     child: Row(
-                  //       children: <Widget>[
-                  //         Container(
-                  //           height: _height * 0.13,
-                  //           width: _width * 0.2,
-                  //           decoration: BoxDecoration(
-                  //             borderRadius: BorderRadius.circular(10.0),
-                  //             image: DecorationImage(
-                  //                 image: AssetImage("assets/1.jpg"),
-                  //                 fit: BoxFit.cover),
-                  //           ),
-                  //         ),
-                  //         SizedBox(width: 25.0),
-                  //         Column(
-                  //           crossAxisAlignment: CrossAxisAlignment.start,
-                  //           children: <Widget>[
-                  //             Text(
-                  //               "Abhiyan Khanal",
-                  //               style: TextStyle(
-                  //                   fontSize: 17.0,
-                  //                   fontWeight: FontWeight.w400,
-                  //                   color: mainCol),
-                  //             ),
-                  //             SizedBox(height: 5.0),
-                  //             Text("Ekantakuna, Kathmandu"),
-                  //           ],
-                  //         )
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              )
+              // child: Column(
+              //   children: <Widget>[
+              //     Container(
+              //       width: _width * 0.95,
+              //       margin: EdgeInsets.all(10.0),
+              //       decoration: BoxDecoration(
+              //         borderRadius: BorderRadius.circular(10.0),
+              //         color: cardCol,
+              //         boxShadow: [
+              //           BoxShadow(
+              //             color: shadowCol.withOpacity(0.7),
+              //             //spreadRadius: 2,
+              //             blurRadius: 9,
+              //             offset: Offset(0, 3), // changes position of shadow
+              //           ),
+              //         ],
+              //       ),
+              //       child: Padding(
+              //         padding: const EdgeInsets.all(10.0),
+              //         child: Row(
+              //           children: <Widget>[
+              //             Container(
+              //               height: _height * 0.13,
+              //               width: _width * 0.2,
+              //               decoration: BoxDecoration(
+              //                 borderRadius: BorderRadius.circular(10.0),
+              //                 image: DecorationImage(
+              //                     image: AssetImage("assets/1.jpg"),
+              //                     fit: BoxFit.cover),
+              //               ),
+              //             ),
+              //             SizedBox(width: 25.0),
+              //             Flexible(
+              //               child: Column(
+              //                 crossAxisAlignment: CrossAxisAlignment.start,
+              //                 children: <Widget>[
+              //                   Text(
+              //                     horo[11]['sunsign'].toUpperCase(),
+              //                     style: TextStyle(
+              //                       fontSize: 16.0,
+              //                       fontWeight: FontWeight.w400,
+              //                       color: mainCol,
+              //                     ),
+              //                   ),
+              //                   SizedBox(height: 5.0),
+              //                   Text(
+              //                     horo[11]['horoscope'],
+              //                     style: TextStyle(
+              //                       fontWeight: FontWeight.w500,
+              //                       fontSize: 13.0,
+              //                     ),
+              //                     maxLines: 3,
+              //                   ),
+              //                 ],
+              //               ),
+              //             )
+              //           ],
+              //         ),
+              //       ),
+              //     ),
+
+              //   ],
+              // ),
               ),
-            ),
-          ]),
-        ),
+        ]),
       );
     } else {
       return Scaffold(
