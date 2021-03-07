88716a07e6b3fb018d114ea8bfa59d1d6f1d2f7a
Abhiyan Khanal <abhiyan5588@gmail.com>
2021-03-07 07:55:06 +0545

    zodiac updated

--- lib/models/horoscope.dart
+++ lib/models/horoscope.dart
@@ -2,8 +2,8 @@ class Horo {
   var date;
   String horoscope;
   String sunshine;
-  String img_url;
-  Horo(this.date, this.horoscope, this.sunshine, this.img_url);
+  //String img_url;
+  Horo(this.date, this.horoscope, this.sunshine);
   // Horo.fromJson(Map<String, dynamic> json) {
   //   date = json['date'];
   //   horoscope = json['horoscope'];
